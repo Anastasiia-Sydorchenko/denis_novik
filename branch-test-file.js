405dc9a7b42fc25ea2986081f5ea281ecf9501bd
Anastasiia <nsydorchenko@gmail.com>
2021-02-02 10:47:00 +0200

    new js file

---
+++ branch-test-file.js
@@ -0,0 +1,8 @@
+(function () {
+   const burger = document.querySelector('.burger');
+   const mobMenu = document.querySelector('.main-menu__mobile');
+   burger.addEventListener('click', () => {
+      burger.classList.toggle('burger-active');
+      mobMenu.classList.toggle('main-menu__mobile_visible');
+   });
+}());
