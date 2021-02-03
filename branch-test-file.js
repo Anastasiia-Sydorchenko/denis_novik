b88144518c6168bcea70641fc1229686df13417c
Anastasiia <nsydorchenko@gmail.com>
2021-02-03 08:25:18 +0200

    add line

--- branch-test-file.js
+++ branch-test-file.js
@@ -1,6 +1,7 @@
 (function () {
    const burger = document.querySelector('.burger');
    const mobMenu = document.querySelector('.main-menu__mobile');
+   
    burger.addEventListener('click', () => {
       burger.classList.toggle('burger-active');
       mobMenu.classList.toggle('main-menu__mobile_visible');
